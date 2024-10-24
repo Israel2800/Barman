9d458b09883f512a4587952a276953d30bd8ab1a
Israel2800 <dady281100@gmail.com>
2024-10-24 00:16:16 -0500

    Working on TableViewController file

--- Drinks+CoreDataClass.swift
+++ Drinks+CoreDataClass.swift
@@ -12,6 +12,14 @@ import CoreData
 @objc(Drinks)
 public class Drinks: NSManagedObject {
     func inicializaCon(_ dict: Dictionary<String, Any>) {
+        let name = (dict["name"] as? String) ?? ""
+        let img = (dict["img"] as? String) ?? ""
+        let ingredients = (dict["ingredients"] as? String) ?? ""
+        let directions = (dict["directions"] as? String) ?? ""
         
+        self.name = name
+        self.img = img
+        self.ingredients = ingredients
+        self.directions = directions
     }
 }
